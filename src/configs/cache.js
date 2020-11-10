261313ee8c621230d22d8e51e05f15fdffefe6b0
mpgxc <mpgx5.c@gmail.com>
2020-11-10 17:17:41 -0300

    chore: remove key password to access redis

--- src/configs/cache.js
+++ src/configs/cache.js
@@ -1,7 +1,6 @@
 require("dotenv/config");
 
 module.exports = {
-    port: process.env.REDIS_PORT,
-    host: process.env.REDIS_HOST,
-    password: process.env.REDIS_PASSWORD,
+  port: process.env.REDIS_PORT,
+  host: process.env.REDIS_HOST,
 };
